813bb0db70e397227a21b7648e44d3c2c010f49b
madisonelyse <50344999+madisonelyse@users.noreply.github.com>
2021-07-06 13:43:34 -0400

    Jquery and Menu
    Add Jquery
    Add mobile menu and styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log("Hello! Thanks for visiting my portfolio site!");
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
